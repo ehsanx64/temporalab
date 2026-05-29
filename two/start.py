c19219a1f03417367cce52df650e480c15dc81ce
Ehsan Mohammadi <ehsanx64@gmail.com>
2026-05-29 16:42:06 +0330

    two: updates in client id string template

--- two/start.py
+++ two/start.py
@@ -3,12 +3,14 @@ import uuid
 from temporalio.client import Client 
 from greeting import Greeting
 
+starter_id = "temporalab/two:start"
+
 async def main():
     client = await Client.connect("localhost:7233")
     result = await client.execute_workflow(
             Greeting.WORKFLOW.value, 
             Greeting.MESSAGE.value,
-            id=f"two::start:{uuid.uuid4()}",
+            id=f"{starter_id}::{uuid.uuid4()}",
             task_queue=Greeting.QUEUE.value,
     )
 
